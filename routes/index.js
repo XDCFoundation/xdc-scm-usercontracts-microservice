1ed62935e39383b9ff98fc1b60ea577d38892848
kajal_KajalBobal <kajal@leewayhertz.com>
2022-03-28 18:31:43 +0530

    swagger imported

--- routes/index.js
+++ routes/index.js
@@ -6,30 +6,38 @@ import ContractModule from "../app/modules/contract";
 import UserModule from "../app/modules/user";
 import {stringConstants} from "../app/common/constants";
 import NetworkModule from "../app/modules/network/index";
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../config/swagger.json';
 
 module.exports = (app) => {
     app.get('/', (req, res) => res.send(stringConstants.SERVICE_STATUS_HTML));
 
+       /**
+     * create swagger UI
+     * **/
+        app.use('/swagger-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
     /**
      * route definition
      */
+    
     app.post("/contract", ValidationManger.addContract, new ContractModule().addContract);
     app.get("/contract",ValidationManger.checkForIdQuery, new ContractModule().getContractById);
-    app.get("/contract/:address", new ContractModule().getContractByAddress);
+    app.put("/contract", new ContractModule().updateContract);
+    app.delete("/contract",ValidationManger.checkForId, new ContractModule().removeContract);
     app.post("/contract-list", new ContractModule().getContractsList);
+    app.post("/contract/:address", new ContractModule().getContractByAddress);
     app.post("/hide-contract", ValidationManger.checkForId, new ContractModule().hideContract);
     app.post("/show-contract",ValidationManger.checkForId,  new ContractModule().showContract);
-    app.delete("/contract",ValidationManger.checkForId, new ContractModule().removeContract);
     app.put("/contract-name",ValidationManger.renameContract , new ContractModule().renameContract);
     app.post("/scm-contracts", new ContractModule().getSCMContracts);
     app.post("/alert-contracts", new ContractModule().getAlertContracts);
-    app.put("/contract", new ContractModule().updateContract);
+    app.get("/check-address", new ContractModule().checkAddress);
 
 
     app.get("/tags", new ContractModule().getListOfTags);
     app.post("/tags",ValidationManger.addTagToContract,  new ContractModule().addTagToContract);
     app.delete("/tags", new ContractModule().removeTagFromContract);
-    app.get("/check-address", new ContractModule().checkAddress);
 
     // User Routes
     app.get("/user", new UserModule().getUserDetails);
