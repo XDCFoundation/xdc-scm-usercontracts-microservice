267917273fdc2a0025e0227fa09bb341aeee0222
kajal_KajalBobal <kajal@leewayhertz.com>
2022-03-29 18:13:05 +0530

    authentication

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,7 @@ import {stringConstants} from "../app/common/constants";
 import NetworkModule from "../app/modules/network/index";
 import swaggerUi from 'swagger-ui-express';
 import swaggerDocument from '../config/swagger.json';
+import {authenticate} from "../middleware/authentication";
 
 module.exports = (app) => {
     app.get('/', (req, res) => res.send(stringConstants.SERVICE_STATUS_HTML));
@@ -21,26 +22,26 @@ module.exports = (app) => {
      * route definition
      */
     
-    app.post("/contract", ValidationManger.addContract, new ContractModule().addContract);
-    app.get("/contract",ValidationManger.checkForIdQuery, new ContractModule().getContractById);
-    app.put("/contract", new ContractModule().updateContract);
-    app.delete("/contract",ValidationManger.checkForId, new ContractModule().removeContract);
-    app.post("/contract-list", new ContractModule().getContractsList);
-    app.post("/contract/:address", new ContractModule().getContractByAddress);
-    app.post("/hide-contract", ValidationManger.checkForId, new ContractModule().hideContract);
-    app.post("/show-contract",ValidationManger.checkForId,  new ContractModule().showContract);
-    app.put("/contract-name",ValidationManger.renameContract , new ContractModule().renameContract);
+    app.post("/contract", authenticate ,ValidationManger.addContract, new ContractModule().addContract);
+    app.get("/contract", authenticate ,ValidationManger.checkForIdQuery, new ContractModule().getContractById);
+    app.put("/contract" , new ContractModule().updateContract);
+    app.delete("/contract",authenticate ,ValidationManger.checkForId, new ContractModule().removeContract);
+    app.post("/contract-list",authenticate, new ContractModule().getContractsList);
+    app.post("/contract/:address", authenticate ,new ContractModule().getContractByAddress);
+    app.post("/hide-contract",authenticate, ValidationManger.checkForId, new ContractModule().hideContract);
+    app.post("/show-contract",authenticate,ValidationManger.checkForId,  new ContractModule().showContract);
+    app.put("/contract-name",authenticate,ValidationManger.renameContract , new ContractModule().renameContract);
     app.post("/scm-contracts", new ContractModule().getSCMContracts);
     app.post("/alert-contracts", new ContractModule().getAlertContracts);
-    app.get("/check-address", new ContractModule().checkAddress);
+    app.get("/check-address",authenticate, new ContractModule().checkAddress);
 
 
-    app.get("/tags", new ContractModule().getListOfTags);
-    app.post("/tags",ValidationManger.addTagToContract,  new ContractModule().addTagToContract);
-    app.delete("/tags", new ContractModule().removeTagFromContract);
+    app.get("/tags",authenticate, new ContractModule().getListOfTags);
+    app.post("/tags",authenticate,ValidationManger.addTagToContract,  new ContractModule().addTagToContract);
+    app.delete("/tags",authenticate, new ContractModule().removeTagFromContract);
 
     // User Routes
-    app.get("/user", new UserModule().getUserDetails);
+    app.get("/user",authenticate, new UserModule().getUserDetails);
     app.post("/user", new UserModule().addUser);
     app.post("/add-network", new NetworkModule().addNetwork);
   app.post("/get-network", new NetworkModule().getNetworksList);
